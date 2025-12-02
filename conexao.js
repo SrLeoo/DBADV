750afb8e1778e254fb6611bde1e32b7f6c72082d
DarioN370 <netobis2015@gmail.com>
2025-12-01 23:26:46 -0300

    test v5

--- conexao.js
+++ conexao.js
@@ -1,25 +1,27 @@
-// Nome do arquivo: conexao.js
+require('dotenv').config(); 
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'process.env.NAME_HOST',
-    user: 'process.env.USER', 
-    password: 'process.env.PASS',
-    database: 'system_logs',
-    port: 'process.env.SQL_PORT'
+    // ATENÇÃO: Sem aspas ('') aqui! Tem que ficar colorido no VS Code.
+    host: process.env.NAME_HOST, 
+    user: process.env.USER,
+    password: process.env.PASS,
+    database: 'system_logs', 
+    port: 7102 
 });
 
 async function salvarLog(company, app, sucesso, falha, total) {
     try {
         const query = `
-            INSERT INTO log_requisicoes 
+            INSERT INTO log_requests 
             (company, aplication, qt_request_successful, qtd_request_fail, qt_request, init_date)
             VALUES (?, ?, ?, ?, ?, NOW())
         `;
+        
         await pool.execute(query, [company, app, sucesso, falha, total]);
-        console.log("✅ Dados salvos no banco com sucesso!");
+        console.log(`[DB] Log salvo na nuvem: S=${sucesso} | F=${falha}`);
     } catch (error) {
-        console.error("❌ Erro ao salvar no banco:", error);
+        console.error("❌ [DB] Erro ao salvar log:", error.message);
     }
 }
 
